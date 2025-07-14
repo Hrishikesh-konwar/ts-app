fd8cdb073f80737d98ccbce5a6c13e17ce1ebb71
Hrishikesh Konwar <53933663+Hrishikesh-konwar@users.noreply.github.com>
2025-07-14 14:31:58 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app : Application = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
